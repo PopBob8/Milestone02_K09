536f7366e927b36a9e5ce164852a282bbe06227b
akmalitb <16520216@std.stei.itb.ac.id>
2021-08-15 11:12:22 +0700

    Menambahkan page Tips

---
+++ idealyfe/src/cmp/card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './card.css';
+
+const Card = (title, desc) => {
+    return (
+        <div className="card">
+            <h2>THIS IS TITLE</h2>
+            <h5>Title description, Sep 2, 2017</h5>
+            {/* <div class="fakeimg" style="height:200px;">Image</div> */}
+            <p>Some text..</p>
+            <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+        </div>
+    )
+}
+
+export default Card;
