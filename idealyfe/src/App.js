2e16ad204ec0b08a2bad0e141718c4ff01093c90
akmalitb <16520216@std.stei.itb.ac.id>
2021-08-15 16:53:50 +0700

    Fix

--- idealyfe/src/App.js
+++ idealyfe/src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import './App.css';
 import Tips from './page/Tips';
-import Default from './page/Default'
+import Default from './page/Default';
 import Button from './cmp/TextButton';
+import Header from './cmp/header';
 
 import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
 
+
+
 function App() {
   return (
+    <div>
+      <Header />
+      <Tips />
+    </div>
   )
 }
 
